8df97c186e01a41002dd3bfdbc522af828230d98
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2022-06-14 22:07:35 +0200

    updating clang-tidy commands

--- run_clang_tidy.sh
+++ run_clang_tidy.sh
@@ -1,3 +1,3 @@
 for file in $(find ./build -name compile_commands.json) ; do
-	run-clang-tidy -fix -header-filter="$CATKIN_WS/src/toulouse_rover/.*" -p $(dirname $file)
+	run-clang-tidy -p $(dirname $file) ./src/toulouse_rover
 done
