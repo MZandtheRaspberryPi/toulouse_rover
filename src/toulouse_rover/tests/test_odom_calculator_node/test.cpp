dd82f25ecc8bcdbc709e296e9c4e43a89ce954c2
MZandtheRaspberryPi <mzandtheraspberrypi@gmail.com>
2022-09-03 11:57:11 +0200

    fixing odom test

--- src/toulouse_rover/tests/test_odom_calculator_node/test.cpp
+++ src/toulouse_rover/tests/test_odom_calculator_node/test.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <math.h>
 #include <ros/ros.h>
+#include <tf2/LinearMath/Quaternion.h>
 
 #include "toulouse_rover/util.h"
 #include "toulouse_rover/WheelSpeeds.h"
@@ -82,6 +83,47 @@ TEST_F(OdomCalculatorFixture, TestCalculatorForward)
   float approximate_x_movement = util::convert_radians_per_sec_to_meters_per_sec(3.141 / 4) * 0.25;
   EXPECT_GT(subsequent_msg.pose.pose.position.x, 0);
   EXPECT_NEAR(subsequent_msg.pose.pose.position.x, approximate_x_movement, 0.003);
+}
+
+TEST_F(OdomCalculatorFixture, TestCalculatorTurnLeft)
+{
+  ros::NodeHandle nh;
+  util::WheelConfigurationType wheel_config_type = util::WheelConfigurationType::DIFFERENTIAL_DRIVE;
+
+  odom_calculator::OdomCalculator odom_calc(nh, wheel_config_type);
+
+  setWheelSpeeds(0, 0, 0, 0);
+
+  while (!got_msg)
+  {
+    ros::spinOnce();
+    ros::Duration(0.01).sleep();
+  }
+
+  nav_msgs::Odometry initialMessage = getMsg();
+  ros::Duration(0.25).sleep();
+
+  setWheelSpeeds(0, 0, 3.141 / 8, 3.141 / 4);
+  while (!got_msg)
+  {
+    ros::spinOnce();
+    ros::Duration(0.01).sleep();
+  }
+
+  nav_msgs::Odometry subsequent_msg = getMsg();
+  float approximate_distance_left_wheel = util::convert_radians_per_sec_to_meters_per_sec(3.141 / 8) * 0.25;
+  float approximate_distance_right_wheel = util::convert_radians_per_sec_to_meters_per_sec(3.141 / 4) * 0.25;
+  float approximate_distance_robot = (approximate_distance_left_wheel + approximate_distance_right_wheel) / 2;
+
+  float approximate_angle_change =
+      (approximate_distance_right_wheel - approximate_distance_left_wheel) / (util::WHEEL_SEP_WIDTH);
+  float approximate_x_change = approximate_distance_robot * cos(0 + approximate_angle_change / 2);
+  float approximate_y_change = approximate_distance_robot * sin(0 + approximate_angle_change / 2);
+  tf2::Quaternion orientation_quaternion;
+  orientation_quaternion.setRPY(0, 0, approximate_angle_change);
+  EXPECT_NEAR(subsequent_msg.pose.pose.position.x, approximate_x_change, 0.003);
+  EXPECT_NEAR(subsequent_msg.pose.pose.position.y, approximate_y_change, 0.003);
+  EXPECT_NEAR(subsequent_msg.pose.pose.orientation.z, orientation_quaternion.getZ(), 0.003);
 }
 
 int main(int argc, char** argv)
